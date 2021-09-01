316402b2c12ce6f3912a61fbb80b38279f20eef0
Gabriel Fairbanks <gabriel.fairbanks@gmail.com>
2021-09-01 08:26:08 +0100

    Initial commit

---
+++ src/test/java/com/fairbanks/netflux/NetfluxApplicationTests.java
@@ -0,0 +1,14 @@
+package com.fairbanks.netflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class NetfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
