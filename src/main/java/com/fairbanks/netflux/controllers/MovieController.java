2361ab14543d310a3794e063938755973b160b35
Gabriel Fairbanks <gabriel.fairbanks@gmail.com>
2021-09-02 07:46:05 +0100

    Created MovieController

---
+++ src/main/java/com/fairbanks/netflux/controllers/MovieController.java
@@ -0,0 +1,29 @@
+package com.fairbanks.netflux.controllers;
+
+import com.fairbanks.netflux.domain.Movie;
+import com.fairbanks.netflux.services.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @GetMapping("/{id}")
+    Mono<Movie> getMovieById(@PathVariable String id){
+        return movieService.getMovieById(id);
+    }
+
+    @GetMapping Flux<Movie> getMovies(){
+        return movieService.getAllMovies();
+    }
+}
